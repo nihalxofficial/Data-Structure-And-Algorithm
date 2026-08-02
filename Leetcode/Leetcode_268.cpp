a1e3fb436d5c78e9e77870aee695e17284a1ce52
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-08-02 20:17:21 +0600

    Leetcode 268 solved

---
+++ Leetcode/Leetcode_268.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int expectedSum=0, arraySum=0;
+        for(int i=0; i<=n; i++){
+            expectedSum+=i;
+        }
+        for(int i=0; i<n; i++){
+            arraySum+=nums[i];
+        }
+
+        return expectedSum-arraySum;
+
+    }
+};
